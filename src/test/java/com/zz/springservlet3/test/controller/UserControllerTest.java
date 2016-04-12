ab4c283019202f7f1ccb0358c969ec7f0fdb1808
yxb_1990 <657048548@qq.com>
2016-04-12 14:38:33 +0800

    添加readme文件，添加测试基类

--- src/test/java/com/zz/springservlet3/test/controller/UserControllerTest.java
+++ src/test/java/com/zz/springservlet3/test/controller/UserControllerTest.java
@@ -1,48 +1,14 @@
 package com.zz.springservlet3.test.controller;
 
 
-import com.zz.springservlet3.config.AppConfig;
-import com.zz.springservlet3.config.web.WebMvcConfig;
-import com.zz.springservlet3.service.UserService;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.test.annotation.IfProfileValue;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {WebMvcConfig.class, AppConfig.class})
-@WebAppConfiguration
-@IfProfileValue(name = "java.vendor", value = "Oracle Corporation")
-public class UserControllerTest {
-
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private MockHttpServletRequest request;
-
-    @Autowired
-    private WebApplicationContext wac;
-
-    private MockMvc mockMvc;
-
-    @Before
-    public void setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac)
-                .build();
-    }
+public class UserControllerTest extends BaseControllerTest {
 
     @Test
     public void get_all() throws Exception {
--- src/test/java/com/zz/springservlet3/test/dao/UserDaoTest.java
+++ src/test/java/com/zz/springservlet3/test/dao/UserDaoTest.java
@@ -1,34 +1,26 @@
 package com.zz.springservlet3.test.dao;
 
-import com.zz.springservlet3.config.PersistenceConfig;
 import com.zz.springservlet3.dao.UserDao;
 import com.zz.springservlet3.entity.User;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@Transactional
-@ContextConfiguration(classes = PersistenceConfig.class)
-public class UserDaoTest {
+public class UserDaoTest extends BaseDaoTest {
 
     @Autowired
     private UserDao userDao;
 
     @Test
-    public void test_user_get(){
+    public void test_user_get() {
         User user = userDao.getOne(1L);
         Assert.assertNotNull(user);
     }
 
     @Test
-    public void insert_user(){
+    public void insert_user() {
         User user = new User();
         user.setId(1L);
         user.setUsername("yxb");
